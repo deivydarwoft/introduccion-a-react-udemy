0e4d6ccfb3573de0f92c0d186c30ae830901b4e9
Deivy <deivy.gutierrez@darwoft.com>
2020-01-21 17:40:40 -0300

    Sección 7: Introducción a React.js (Actualización de la Seccion 2) (Finalizado)

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,50 @@
+import {BasicPicker} from "react-color-tools"
 import ClaseComponent from "../componentes/ClaseComponent"
-
 import getHistoryData from "../JSON/getHistoryData"
-
 import getMathData from "../JSON/getMathData"
-
-export default () => (
-	<div>
-		<h1 style={{textAlign:"center"}}>Curso del plan</h1>
-		<div style={{display:"flex", justifyContent:"space-around"}}>
-			<ClaseComponent nombre="matemática" clase = {getMathData}/>
-			<ClaseComponent nombre="historia" clase = {getHistoryData}/>
-		</div>
-		<style jsx>
-		{`
-			body{
-				background: #ffca20;
-			}
-		`}
-		</style>
+const Contenedor = (props) => (
+	<div 
+		style={{
+			"backgroundColor": props.color
+		}}>
+		{props.children}
 	</div>
+
 )
+class Index extends React.Component {
+	state = {
+		color1: " #ffca20",
+		color2: " #088da5"
+	}
+	render(){
+		const {color1, color2} = this.state
+		return (
+			<React.Fragment>
+				<Contenedor color={color1}>
+					<h1 style={{textAlign:"center"}}>Curso del plan</h1>
+					<div style={{display:"flex", justifyContent:"space-around"}}>
+						<ClaseComponent nombre="matemática" clase = {getMathData}/>
+						<ClaseComponent nombre="historia" clase = {getHistoryData}/>
+					</div>
+					<BasicPicker
+						color={this.state.color}
+						onChange={(color1)=>this.setState({color1})}
+					/>
+				</Contenedor>
+				<Contenedor color={color2}>
+					<h1 style={{textAlign:"center"}}>Curso del plan</h1>
+					<div style={{display:"flex", justifyContent:"space-around"}}>
+						<ClaseComponent nombre="matemática" clase = {getMathData}/>
+						<ClaseComponent nombre="historia" clase = {getHistoryData}/>
+					</div>
+					<BasicPicker
+						color={this.state.color}
+						onChange={(color2)=>this.setState({color2})}
+					/>
+				</Contenedor>
+				</React.Fragment>
+		)
+	}
+	
+}
+export default Index
