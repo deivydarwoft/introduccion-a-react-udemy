9afe73495ba8de3692441d2036e1157cce27a72e
Deivy <deivy.gutierrez@darwoft.com>
2020-01-24 15:47:47 -0300

    Sección 3: Interactividad en React

--- componentes/CalculadoraComponent.js
+++ componentes/CalculadoraComponent.js
@@ -1,6 +1,19 @@
 class Calculadora extends React.Component{
     state ={
-        res:"0"
+        res:""
+    }
+    onDigit = (digit) => {
+        const expresion = this.state.res + digit
+        this.setState({
+            res:expresion
+        })
+    }
+    ejecutarOperacion = () => {
+        const resultados = eval(this.state.res)
+        console.log(resultados)
+        this.setState({
+            res:resultados
+        })
     }
     
     render(){
@@ -10,12 +23,27 @@ class Calculadora extends React.Component{
             <div>
                 <h1>Calculadora</h1>
                 <div className="z-depth-2 teal white-text center-align resultado">
-                    {this.state.res}
+                    {this.state.res || "0"}
                 </div>
-                <div style={{textAlign:"center"}}>
-                    <a className="waves-effect waves-light btn" onClick={alertar}>0</a>
-                    <a className="waves-effect waves-light btn">1</a>
-                    <a className="waves-effect waves-light btn">2</a>
+                <div className="calc-container blue lighten-3 hoverable z-depth-2">
+                    {
+                        ["1","2","3","4","5","6","7","8","9","0","."].map(digit => {
+                            return (
+                            <a className="waves-effect waves-light btn" onClick={()=>this.onDigit(digit)}>{digit}</a>
+                            )
+                        })  
+                    }
+                </div>
+                <div className="operadores center-align">
+                    {
+                        ["+","-","*","/"].map(digit => {
+                            return (
+                            <a className="waves-effect waves-light btn deep-orange darken-1" onClick={()=>this.onDigit(digit)}>{digit}</a>
+                            )
+                        })  
+                    }
+                    <a className="waves-effect waves-light btn deep-orange darken-1" onClick={()=>this.setState({res:""})}>C</a>
+                    <a className="waves-effect waves-light btn deep-orange darken-1" onClick={()=>this.ejecutarOperacion()}>=</a>
                 </div>
                 <style jsx>
                     {`
@@ -24,6 +52,20 @@ class Calculadora extends React.Component{
                             margin: 0 auto;
                             font-size: 32px;
                             margin-bottom: 10px
+                        }
+                        .calc-container{
+                            width: 300px;
+                            margin: 0 auto;
+                            text-align: center;
+                            padding: 12px;
+                        }
+                        .calc-container a{
+                            margin: 5px;
+                        }
+                        .operadores{
+                            border: 1px solid #ccc;
+                            padding: 10px;
+                            margin-top:20px;
                         }
                     `}
                 </style>
